7244542efaaf0c4dd36fcc3698de9d289855c77c
Tristan Rice <rice@fn.lc>
2015-08-12 00:41:08 -0700

    Updates

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,5 +10,9 @@ set :repository, 'https://github.com/d4l3k/fn.lc.git'
 task :deploy do
     deploy do
         invoke :'git:clone'
+        queue 'ls'
+        queue 'npm install'
+        queue 'bower install'
+        queue 'gulp'
     end
 end
